8239cf558c866070f147785e2bd4be45aa344ef8
Morgan Newman <mpnewman95@gmail.com>
2018-12-04 10:06:44 -0800

    Adds redux to the controller layer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import { Provider } from 'react-redux';
+import store from './controller/store';
+import App from './app/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
