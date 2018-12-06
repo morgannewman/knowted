b8c36e5e96c99906444b7542f56060a593721b81
Morgan Newman <mpnewman95@gmail.com>
2018-12-06 11:04:21 -0800

    Adds api layer to controller

--- src/controller/actions/auth.js
+++ src/controller/actions/auth.js
@@ -1,3 +1,5 @@
+import api from '../../controller/api';
+
 export const AUTH_SUBMIT = 'AUTH_SUBMIT';
 export const AUTH_SUCCESS = 'AUTH_SUCCESS';
 export const AUTH_ERROR = 'AUTH_ERROR';
@@ -11,7 +13,24 @@ export const AUTH_ERROR = 'AUTH_ERROR';
  */
 export const submitAuthLogin = credentials => dispatch => {
 	dispatch(authSubmit());
-	Promise.all(console.log('calling API here with', credentials))
+	api.auth
+		.login(credentials)
+		.then(user => authSuccess(user))
+		.catch(err => authError(err));
+};
+
+/**
+ * Components can consume this function to register AND login via name/email/password.
+ * On submit: state.auth.submitting === true
+ * On success: state.auth.loggedIn === true
+ * On fail: state.auth.error === some error object
+ * @param {{email: string, password: string}} credentials
+ */
+export const submitAuthRegistration = credentials => dispatch => {
+	dispatch(authSubmit());
+	api.auth
+		.register(credentials)
+		.then(api.auth.login(credentials))
 		.then(user => authSuccess(user))
 		.catch(err => authError(err));
 };
---
+++ src/controller/api/config.js
@@ -0,0 +1,54 @@
+import cache from './cache';
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080/';
+
+/**
+ * Normalizes the behavior of fetch calls to RESTful APIs that return JSON.
+ * @param {String} relativePath - the relative path for the AJAX call (e.g. /api/auth)
+ * @param {{ method: string, auth: boolean | true, }} config - Configuration variables for fetch. Defaults to GET.
+ * @param {null | {param: string}} params - An object of query parameters or none.
+ * @returns the decoded JSON response.
+ * @throws if response is not a 200-level response (i.e. fetch's res.ok is false).
+ */
+export function goFetch(relativePath, config = { method: 'get' }, params = null) {
+	const url = new URL(BACKEND_URL + relativePath);
+	if (params !== null) Object.entries(params).forEach(([k, v]) => url.searchParams.append(k, v));
+	// set sensible default options
+	const options = {
+		headers: {
+			'Content-Type': 'application/json; charset=utf-8'
+		},
+		mode: 'cors',
+		auth: 'true',
+		...config
+	};
+	// Serialize body if necessary
+	if (options.body && typeof options.body === 'object') options.body = JSON.stringify(options.body);
+	// Set auth header
+	if (options.auth) {
+		const token = cache.authToken.load();
+		if (!token) {
+			return Promise.reject({
+				code: 400,
+				message: 'Missing auth token for Authorization header.'
+			});
+		}
+		delete options.token;
+		options.headers.Authorization = `Bearer ${token}`;
+	}
+	return fetch(url, options).then(res => {
+		if (!res.ok) {
+			if (res.headers.has('content-type') && res.headers.get('content-type').startsWith('application/json')) {
+				// It's a nice JSON error returned by us, so decode it
+				return res.json().then(err => Promise.reject(err));
+			}
+			// It's a less informative error returned by express
+			return Promise.reject({
+				code: res.status,
+				message: res.statusText
+			});
+		}
+		if (res.headers.has('content-type') && res.headers.get('content-type').startsWith('application/json'))
+			return res.json();
+		else return res;
+	});
+}
