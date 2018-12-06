accb7e0c627a8ad1d2b1677964cd2607cc85b894
clkent <chelsea.kent@gmail.com>
2018-12-05 22:08:32 -0800

    built out jaw bone and connected folder onClick to open it with appropriate topics

--- src/app/Dashboard/AllTopicsContainer.js
+++ src/app/Dashboard/AllTopicsContainer.js
@@ -2,48 +2,24 @@ import React from 'react';
 import Folder from './Folder';
 import Topic from './Topic';
 
+//FIXME: remove hard coded references
+import TopicsData from '../../db/topicsData';
+import FoldersData from '../../db/foldersData';
+
 class AllTopicsContainer extends React.Component {
-  //FIXME: hard coded for now
-  topics = [
-    { id: 1, title: 'Topic One', parent: null },
-    {
-      id: 2,
-      title: 'Topic Two',
-      parent: 101
-    },
-    {
-      id: 3,
-      title: 'Topic Three',
-      parent: 101
-    }
-  ];
-
-  folders = [{ id: 101, title: 'Folder One' }];
-
-  renderTopics(topic) {
-    //if topic.parent is null
-    if (!topic.parent) {
-      return <Topic title={topic.title} key={topic.id} />;
-    }
-    // let folderId = topic.parent;
-    // return this.findFolder(folderId);
-  }
-
-  // findFolder(folderId) {
-  //   //find folder based on ID
-  //   let result = this.folders.find(folder => folder.id === folderId);
-  //   return <Folder title={result.title} />;
-  // }
-
   render() {
     return (
       <React.Fragment>
-        <button>add topic</button>
+        <button onClick={() => console.log('add a topic')}>add topic</button>
         <br />
-        {this.folders.map((folder, index) => {
-          return <Folder title={folder.title} key={index} />;
+        {FoldersData.map((folder, index) => {
+          return <Folder title={folder.title} key={index} id={folder.id} />;
         })}
-        {this.topics.map(topic => this.renderTopics(topic))}
+        {TopicsData.map(topic =>
+          !topic.parent ? (
+            <Topic title={topic.title} key={topic.id} topicId={topic.id} />
+          ) : null
+        )}
       </React.Fragment>
     );
   }
