cf444854a5f8c44d71205e587b95886acbe8f426
Janet Leon <leon.irenejanet@gmail.com>
2018-12-06 13:52:05 -0800

    Resets app.test to match master

--- src/app/__tests__/App.test.js
+++ src/app/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
-import TopicDashboard from '../TopicDasboard';
+import { App } from '../App';
 
 it('renders without crashing', () => {
-  shallow(<TopicDashboard />);
+	shallow(<App />);
 });
