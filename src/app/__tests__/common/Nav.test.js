f8db1226bbf277cc5d51e9eb9db858eb0b57a1f7
Morgan Newman <mpnewman95@gmail.com>
2018-12-06 10:14:16 -0800

    Updates Nav test import

--- src/app/__tests__/common/Nav.test.js
+++ src/app/__tests__/common/Nav.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Nav } from '../../common/nav';
+import { Nav } from '../../common/Nav';
 
 describe('<Nav />', () => {
 	it('renders without crashing', () => {
