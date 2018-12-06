65425817959a8e302df54d2c7b587613c8d2d095
Janet Leon <leon.irenejanet@gmail.com>
2018-12-06 11:08:22 -0800

    sets up TopicDashboard and test placeholders

---
+++ src/app/TopicDasboard/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class TopicDashboard extends React.Component {
+  render() {
+    return (
+      <main>
+        <section>
+          <h2>Breadcrumb nav placeholder</h2>
+        </section>
+        <section>
+          <h2>Active Resource Container placeholder</h2>
+        </section>
+        <section>
+          <h2>Completed Resource Container placeholder</h2>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default TopicDashboard;
