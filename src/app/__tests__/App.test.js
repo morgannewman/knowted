a3aa09dcd9395d35bc37e0f94cc423a438dfe012
Janet Leon <leon.irenejanet@gmail.com>
2018-12-06 11:18:20 -0800

    Fixes typos

--- src/app/__tests__/App.test.js
+++ src/app/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
-import { App } from '../App';
+import TopicDashboard from '../TopicDasboard';
 
 it('renders without crashing', () => {
-	shallow(<App />);
+  shallow(<TopicDashboard />);
 });
