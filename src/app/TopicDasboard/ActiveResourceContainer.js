5ce51b635e5a357c6b5452befb1f1f3d9edface9
Janet Leon <leon.irenejanet@gmail.com>
2018-12-07 13:51:00 -0800

    first child has special styling

--- src/app/TopicDasboard/ActiveResourceContainer.js
+++ src/app/TopicDasboard/ActiveResourceContainer.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ResourceItem from './ResourceItem';
+import './ActiveResourceContainer.scss';
 export class ActiveResourceContainer extends React.Component {
+  //TODO: Drag and drop functionality
+  componentDidMount() {
+    console.log('component mounts and ready to dispatch actions');
+  }
   render() {
     const { resources } = this.props;
 
     return (
-      <section className="active-resources">
-        <ul>
+      <section className="active-resources-container">
+        <ul className="active-resources-list">
           {resources.map(rescItem => {
             console.log(rescItem);
             if (!rescItem.resource.completed) {
               return (
-                <li key={rescItem.resource.id}>
+                <li
+                  key={rescItem.resource.id}
+                  className="resource-item-container"
+                >
                   <ResourceItem resource={rescItem.resource} />
                 </li>
               );
