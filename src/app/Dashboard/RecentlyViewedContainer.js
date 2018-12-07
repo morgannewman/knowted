8b1040d910e8f0b6dfab0b0a497fc85aa8496429
Janet Leon <leon.irenejanet@gmail.com>
2018-12-06 23:33:19 -0800

    deletes unused import 'topics data

--- src/app/Dashboard/RecentlyViewedContainer.js
+++ src/app/Dashboard/RecentlyViewedContainer.js
@@ -4,7 +4,6 @@ import React, { Component } from 'react';
 import RecentResourceItem from './RecentResourceItem';
 
 //TODO: delete these two imports once we're connected and can pass through state
-import topicsData from '../../dummyDB/topicsData';
 import resourcesData from '../../dummyDB/resourcesData';
 
 export class RecentlyViewed extends Component {
