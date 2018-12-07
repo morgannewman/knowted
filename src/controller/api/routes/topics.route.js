f702cf37fc6ba98d02169eedd4532c6b8711e7ce
Morgan Newman <mpnewman95@gmail.com>
2018-12-07 09:28:51 -0800

    Finishes implementing prototype of interface

--- src/controller/api/routes/topics.route.js
+++ src/controller/api/routes/topics.route.js
@@ -1,4 +1,46 @@
 import { goFetch } from '../config';
 const BASE_URL = '/api/topics';
 
-export default {};
+export default {
+	/**
+	 * Exposes GET /api/topics.
+	 * Topics have these props:
+	 * id, title, parent, lastOpened, notebook,
+	 * resourceOrder, created_at, updated_at.
+	 * @returns {[{}]} an array of topic objects
+	 */
+	get() {
+		return goFetch(BASE_URL);
+	},
+
+	/**
+	 * Exposes POST /api/topics.
+	 * Topics can be created with these props:
+	 * title, parent, notebook
+	 * @param {{}} body
+	 * @returns {[{}]} an array of topic objects
+	 */
+	post(body) {
+		return goFetch(BASE_URL, { method: 'POST', body });
+	},
+
+	/**
+	 * Exposes PUT /api/topics.
+	 * Topics can be updated with these props:
+	 * title, parent, notebook, lastOpened, resourceOrder
+	 * @param {{}} body
+	 * @returns {[{}]} an array of topic objects
+	 */
+	put(body) {
+		if (!body.id) throw new Error("Missing topic's `id` in request body.");
+		return goFetch(`${BASE_URL}/${body.id}`, { method: 'PUT', body });
+	},
+
+	/**
+	 * Exposes DELETE /api/topics/:id.
+	 * @param {number | string} id
+	 */
+	delete(id) {
+		return goFetch(`${BASE_URL}/${id}`, { method: 'DELETE' });
+	}
+};
