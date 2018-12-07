7ef92b9a509b549bd94a08ad956554a245bbf399
Janet Leon <leon.irenejanet@gmail.com>
2018-12-07 08:20:25 -0800

    tests for functions to be called on edit and delete buttons

--- src/app/__tests__/TopicDashboard/ResourceData.test.js
+++ src/app/__tests__/TopicDashboard/ResourceData.test.js
@@ -36,4 +36,47 @@ describe('<ResourceData />', () => {
         .text()
     ).toEqual(FirstResource.resource.uri);
   });
+
+  it('contains an edit button', () => {
+    expect(
+      wrapper
+        .find('button')
+        .first()
+        .text()
+    ).toEqual('edit');
+  });
+  it('contains a delete button', () => {
+    expect(
+      wrapper
+        .find('button')
+        .last()
+        .text()
+    ).toEqual('delete');
+  });
+
+  it('edit triggers an edit function', () => {
+    let wrapper;
+    const mockFn = jest.fn();
+    wrapper = shallow(
+      <ResourceData handleEdit={mockFn} resource={FirstResource.resource} />
+    );
+    wrapper
+      .find('button')
+      .first()
+      .simulate('click');
+    expect(mockFn).toBeCalled();
+  });
+
+  it('delete triggers a delete function', () => {
+    let wrapper;
+    const mockFn = jest.fn();
+    wrapper = shallow(
+      <ResourceData handleDelete={mockFn} resource={FirstResource.resource} />
+    );
+    wrapper
+      .find('button')
+      .last()
+      .simulate('click');
+    expect(mockFn).toBeCalled();
+  });
 });
