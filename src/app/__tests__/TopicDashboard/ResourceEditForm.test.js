b009335f482e4838865a83a49beedb4891a207e0
Janet Leon <leon.irenejanet@gmail.com>
2018-12-07 08:51:49 -0800

    tests that form submits

--- src/app/__tests__/TopicDashboard/ResourceEditForm.test.js
+++ src/app/__tests__/TopicDashboard/ResourceEditForm.test.js
@@ -12,4 +12,42 @@ describe('<ResourceEditForm', () => {
     wrapper = shallow(<ResourceEditForm resource={FirstResource.resource} />);
   });
   it('renders without crash', () => {});
+
+  it('renders a form', () => {
+    expect(wrapper.find('form')).toBeTruthy();
+  });
+
+  it('renders a text input', () => {
+    expect(wrapper.find('form input')).toBeTruthy();
+    expect(wrapper.find('form input').type()).toEqual('input');
+  });
+
+  it('renders an update button', () => {
+    expect(wrapper.find('button')).toBeTruthy();
+    expect(wrapper.find('button').text()).toEqual('update');
+  });
+
+  it('form button triggers submit', () => {
+    let wrapper;
+    const mockFn = jest.fn();
+    const fakeEvent = {
+      preventDefault: () => {
+        console.log('preventDefault');
+      },
+      currentTarget: {
+        getElementsByTagName: () => {
+          console.log('I am a fake element');
+          return ['fake Element'];
+        }
+      }
+    };
+    wrapper = shallow(
+      <ResourceEditForm
+        handleUpdate={mockFn}
+        resource={FirstResource.resource}
+      />
+    );
+    wrapper.find('form').simulate('submit', fakeEvent);
+    expect(mockFn).toBeCalled();
+  });
 });
