2207b88878e15577c1df0e125f73e361ffd52333
clkent <chelsea.kent@gmail.com>
2018-12-07 13:03:17 -0800

    updating topics action

--- src/controller/actions/topic.js
+++ src/controller/actions/topic.js
@@ -1,8 +1,25 @@
+import api from '../../controller/api';
+
 export const TOPIC_SUBMIT = 'TOPIC_SUBMIT';
 export const TOPIC_SUCCESS = 'TOPIC_SUCCESS';
 export const TOPIC_ERROR = 'TOPIC_ERROR';
 
 export const TOPIC_DELETE = 'TOPIC_DELETE';
+
+/**
+ * Components can consume this function to add a new Topic
+ * On submit: state.topic.loading === true
+ * On success: state.topic.topics === [of topics]
+ * On fail: state.topic.error === some error object
+ * @param {{title: string}} credentials
+ */
+export const submitTopic = credentials => dispatch => {
+  dispatch(topicSubmit());
+  api.auth
+    .login(credentials)
+    .then(title => dispatch(topicSuccess(title)))
+    .catch(err => dispatch(topicError(err)));
+};
 
 export const topicSubmit = () => ({
   type: TOPIC_SUBMIT
