0c7176f949a95c9d36da0e7e20bd7e2f32abf9c6
clkent <chelsea.kent@gmail.com>
2018-12-07 10:38:51 -0800

    add topic reducers and actions

--- src/app/Dashboard/Topic.js
+++ src/app/Dashboard/Topic.js
@@ -3,13 +3,9 @@ import PropTypes from 'prop-types';
 import './Topic.css';
 import { connect } from 'react-redux';
 
-<<<<<<< HEAD
 import { deleteTopic } from '../../controller/actions/topic';
 
 export class Topic extends React.Component {
-=======
-export default class Topic extends React.Component {
->>>>>>> combined some components to simplify and set up initial action and reducers for Folders
   static propTypes = {
     topicId: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired
@@ -19,7 +15,6 @@ export default class Topic extends React.Component {
     isHidden: true
   };
 
-<<<<<<< HEAD
   editTopic = () => {
     console.log('edit:', this.props.topicId);
   };
@@ -29,17 +24,6 @@ export default class Topic extends React.Component {
   };
 
   toggleHidden = () => {
-=======
-  editTopic(topicId) {
-    console.log('edit:', topicId);
-  }
-
-  deleteTopic(topicId) {
-    console.log('delete:', topicId);
-  }
-
-  toggleHidden() {
->>>>>>> combined some components to simplify and set up initial action and reducers for Folders
     this.setState({
       isHidden: !this.state.isHidden
     });
@@ -60,17 +44,10 @@ export default class Topic extends React.Component {
         >
           {title}
         </button>
-<<<<<<< HEAD
         {!this.state.isHidden && (
           <div className="edit-delete-topic-options">
             <button onClick={this.editTopic}>Edit</button>
             <button onClick={this.deleteTopic}>Delete</button>
-=======
-        {this.state.isHidden ? null : (
-          <div className="edit-delete-topic-options">
-            <button onClick={() => this.editTopic(topicId)}>Edit</button>
-            <button onClick={() => this.deleteTopic(topicId)}>Delete</button>
->>>>>>> combined some components to simplify and set up initial action and reducers for Folders
           </div>
         )}
       </div>
