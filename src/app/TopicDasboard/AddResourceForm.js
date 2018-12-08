e610a38920a299a51328dec6d27b5001cd283381
Janet Leon <leon.irenejanet@gmail.com>
2018-12-07 18:39:27 -0800

    takes in input values from user

--- src/app/TopicDasboard/AddResourceForm.js
+++ src/app/TopicDasboard/AddResourceForm.js
@@ -3,16 +3,27 @@ import React from 'react';
 export class AddResourceForm extends React.Component {
   handleSubmit = e => {
     e.preventDefault();
-    console.log('form submits');
+    console.log('Title', this.inputTitle.value);
+    console.log('URI', this.inputUri.value);
   };
   render() {
     return (
       <section className="add-resource-section">
         <form className="add-resource-form" onSubmit={this.handleSubmit}>
           <label htmlFor="add-resource-link">Add New Reource</label>
-          <input type="text" name="add-resource" placeholer="htttp:// " />
+          <input
+            ref={input => (this.inputUri = input)}
+            type="text"
+            name="add-resource"
+            placeholder="http://"
+          />
           <label htmlFor="add-resource-title">Add New Reource</label>
-          <input type="text" name="add-resource" placeholer="title " />
+          <input
+            ref={input => (this.inputTitle = input)}
+            type="text"
+            name="add-resource"
+            placeholder="title "
+          />
           <button>Add New Resource</button>
         </form>
       </section>
