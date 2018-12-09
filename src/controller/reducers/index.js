aecee0a46cf5f53f2cf44956380a1493c8b02021
clkent <chelsea.kent@gmail.com>
2018-12-08 22:02:08 -0800

    connecting UI to get all topics and setting up folder actions

--- src/controller/reducers/index.js
+++ src/controller/reducers/index.js
@@ -1,9 +1,15 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
+<<<<<<< HEAD
 
 import folderReducer from './folder';
 import topicReducer from './topic';
 
+=======
+import folderReducer from './folder';
+import topicReducer from './topic';
+
+>>>>>>> connecting UI to get all topics and setting up folder actions
 const rootReducer = combineReducers({ auth, folderReducer, topicReducer });
 
 export default rootReducer;
