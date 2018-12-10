71efcd9c0078d3b6eb320eff246d3117a71c6c6d
Janet Leon <leon.irenejanet@gmail.com>
2018-12-10 09:08:32 -0800

    creates resource action and controllers

--- src/controller/actions/resource.js
+++ src/controller/actions/resource.js
@@ -1,1 +1,14 @@
 import api from '../../controller/api';
+
+export const RESOURCE_LOADING = 'RESOURCE_LOADING';
+export const resource_Loading = () => ({
+  type: RESOURCE_LOADING
+});
+
+export const RESOURCE_SUCCESS = 'RESOURCE_SUCCESS';
+export const resource_success = resources => ({
+  type: RESOURCE_SUCCESS,
+  payload: resources
+});
+
+export const get_resources = id => dispatch => {};
--- src/controller/reducers/resource.js
+++ src/controller/reducers/resource.js
@@ -1,6 +1,26 @@
+import { RESOURCE_LOADING } from '../actions/resource';
+import produce from 'immer';
+
 export const initialState = {
   resources: [],
   loading: false,
   error: null,
   folderId: null
 };
+
+export default resource((state, action) => {
+  switch (action.type) {
+    case RESOURCE_LOADING:
+      state.loading = true;
+      state.error = null;
+      return;
+    // case RESOURCE_SUCCESS:
+    //   state.loading = false;
+    //   state.error = null;
+    //   state.resources = action.payload;
+    //   return;
+
+    default:
+      return;
+  }
+}, initialState);
