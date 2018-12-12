afa5105a7d1d9646eace719c7afe768eb8d639b8
Janet Leon <leon.irenejanet@gmail.com>
2018-12-12 07:49:00 -0800

    FIX: Loading should not display if resources are 0

--- src/app/TopicDasboard/ActiveResourceContainer.js
+++ src/app/TopicDasboard/ActiveResourceContainer.js
@@ -7,35 +7,31 @@ import Loading from '../common/Loading';
 export class ActiveResourceContainer extends React.Component {
   //TODO: Drag and drop functionality
 
-  //FIXME: ADD Zero Case
   render() {
     const { resources } = this.props;
 
-    if (resources.length < 0) {
-      return <Loading />;
-    } else {
-      return (
-        <section className="active-resources-container">
-          <ul className="active-resources-list">
-            {resources.map(rescItem => {
-              if (rescItem && !rescItem.completed) {
-                return (
-                  <li key={rescItem.id} className="resource-item-container">
-                    <ResourceItem resource={rescItem} />
-                  </li>
-                );
-              } else {
-                return null;
-              }
-            })}
-            <AddResourceForm />
-          </ul>
-        </section>
-      );
-    }
+    return (
+      <section className="active-resources-container">
+        <ul className="active-resources-list">
+          {resources.map(rescItem => {
+            if (rescItem && !rescItem.completed) {
+              return (
+                <li key={rescItem.id} className="resource-item-container">
+                  <ResourceItem resource={rescItem} />
+                </li>
+              );
+            } else {
+              return null;
+            }
+          })}
+          <AddResourceForm />
+        </ul>
+      </section>
+    );
   }
 }
 
+//FIXME: This component might not need to be connected?
 // const mapStateToProps = state => {
 //   return {
 //     loading: state.resourceReducer.loading,
