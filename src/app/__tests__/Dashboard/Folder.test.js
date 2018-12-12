dcc9fb38d26dae4ee36891e25d613f724de6fc80
clkent <chelsea.kent@gmail.com>
2018-12-12 08:11:56 -0800

    fixing tests

--- src/app/__tests__/Dashboard/Folder.test.js
+++ src/app/__tests__/Dashboard/Folder.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Folder from '../../Dashboard/Folder';
+import { Folder } from '../../Dashboard/Folder';
 
 //Component Contract - expect:
 //To render
