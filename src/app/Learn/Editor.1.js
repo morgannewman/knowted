ff45b5cec36cc88fe23029029313e55803a2c9f0
Morgan Newman <mpnewman95@gmail.com>
2018-12-14 09:37:59 -0800

    Reverts the editor back to original. Saving progress in new file

--- src/app/Learn/Editor.1.js
+++ src/app/Learn/Editor.1.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import hljs from 'highlight.js';
 import 'react-quill/dist/quill.snow.css';
 import Quill from 'react-quill';
 import { connect } from 'react-redux';
@@ -6,6 +7,10 @@ import PropTypes from 'prop-types';
 import debounce from 'lodash.debounce';
 import { submitNotebookUpdate } from '../../controller/actions/learn';
 import cache from '../../controller/api/cache';
+
+hljs.configure({   // optionally configure hljs
+  languages: ['javascript', 'ruby', 'python']
+});
 
 export class Editor extends React.Component {
 	static propTypes = {
@@ -20,6 +25,8 @@ export class Editor extends React.Component {
 		hasPendingUpdate: false
 	};
 
+	editorRef = React.createRef();
+
 	componentDidMount() {
 		window.addEventListener('unload', this.handleWindowClose);
 		// Initialize the initialText value from props or as empty editor
@@ -33,6 +40,9 @@ export class Editor extends React.Component {
 			2000,
 			{ maxWait: 10000 }
 		);
+		
+		const docLength = this.editorRef.getEditor().getLength();
+		this.editorRef.getEditor().insertText(docLength === 1 ? 0 : docLength,'TEST\n', { bold: true, header: 1, underline: true });
 	}
 
 	handleWindowClose = () => {
@@ -51,15 +61,36 @@ export class Editor extends React.Component {
 
 	handleChange = (content, delta, source, editor) => {
 		const lastUpdate = editor.getContents();
+		console.log(lastUpdate);
 		this.setState({ lastUpdate, text: content, hasPendingUpdate: true });
 		this.save(lastUpdate);
 	};
 
 	render() {
 		const { isDisabled, text } = this.state;
+		const toolbar = [['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+		['blockquote', 'code-block'],
+	
+		[{ 'header': 1 }, { 'header': 2 }],               // custom button values
+		[{ 'list': 'ordered'}, { 'list': 'bullet' }],
+		[{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+		[{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+		[{ 'direction': 'rtl' }],                         // text direction
+	
+		[{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+		[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+	
+		[{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+		[{ 'font': [] }],
+		[{ 'align': [] }],
+	
+		['clean'] ];
 
 		return (
-			<Quill value={text} readOnly={isDisabled} onChange={this.handleChange} className="editor" theme="snow" />
+			<Quill value={text} modules={{
+				syntax: true,
+				toolbar
+			}} readOnly={isDisabled} onChange={this.handleChange} className="editor" theme="snow" ref={(element) => this.editorRef = element} />
 		);
 	}
 }
