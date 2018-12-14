01f2cb2222960625b2d8f57483d564e3cd8cb4cb
Morgan Newman <mpnewman95@gmail.com>
2018-12-14 10:01:56 -0800

    Refactors initialize action and reducer

--- src/controller/reducers/learn.js
+++ src/controller/reducers/learn.js
@@ -1,8 +1,7 @@
 import produce from 'immer';
 import {
 	LEARN_RESET,
-	LEARN_SET_TOPIC,
-	LEARN_SET_RESOURCE,
+	LEARN_INITIALIZE_SUCCESS,
 	LEARN_API_ERROR,
 	LEARN_NOTEBOOK_UPDATE_SUBMIT,
 	LEARN_NOTEBOOK_UPDATE_SUCCESS,
@@ -18,19 +17,20 @@ const initialState = {
 
 export default produce((state, action) => {
 	switch (action.type) {
+		case LEARN_INITIALIZE_SUCCESS:
+			const { topic, resource } = action.payload;
+			state.topic = topic;
+			state.resource = resource;
+
+			state.loading = false;
+			state.error = null;
+			return;
+
 		case LEARN_RESET:
 			return { ...initialState };
 
 		case LEARN_API_ERROR:
 			return { ...initialState, error: action.payload };
-
-		case LEARN_SET_TOPIC:
-			state.topic = action.payload;
-			return;
-
-		case LEARN_SET_RESOURCE:
-			state.resource = action.payload;
-			return;
 
 		case LEARN_NOTEBOOK_UPDATE_SUBMIT:
 			state.error = null;
