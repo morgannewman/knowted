02a9c494ef044d4c1a157f1d3daa96584d4f4669
Morgan Newman <mpnewman95@gmail.com>
2018-12-17 08:41:31 -0800

    Adds users route to interface

--- src/controller/api/index.js
+++ src/controller/api/index.js
@@ -3,15 +3,18 @@ import topics from './routes/topics.route';
 import folders from './routes/folders.route';
 import resources from './routes/resources.route';
 import metadata from './routes/metadata.route';
+import users from './routes/users.route';
+
 /**
  * This is an interface that provides access to the backend endpoints.
  */
 const api = {
-  auth,
-  topics,
-  resources,
-  folders,
-  metadata
+	auth,
+	topics,
+	resources,
+	folders,
+	metadata,
+	users
 };
 
 export default api;
