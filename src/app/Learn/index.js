f436f647135b23e98fb49f45c5a13f6e6e2a1442
Morgan Newman <mpnewman95@gmail.com>
2018-12-17 08:01:41 -0800

    Small style fixes

--- src/app/Learn/index.js
+++ src/app/Learn/index.js
@@ -8,8 +8,7 @@ import { Redirect } from 'react-router-dom';
 
 import { initializeLearn, resetLearn } from '../../controller/actions/learn';
 import Loading from '../common/Loading';
-
-import BreadCrumb from '../common/Breadcrumb';
+import Breadcrumbs from '../common/Breadcrumbs';
 
 export class Learn extends React.Component {
 	static propTypes = {
@@ -22,7 +21,7 @@ export class Learn extends React.Component {
 	}
 
 	render() {
-		const { stateIsStale, loading, resourceNotFound, notebook, topicTitle, topicId, resourceId, resourceTitle } = this.props;
+		const { stateIsStale, loading, resourceNotFound, notebook, topic, resource } = this.props;
 
 		if (resourceNotFound) {
 			this.props.dispatch(resetLearn());
@@ -33,7 +32,12 @@ export class Learn extends React.Component {
 
 		return (
 			<>
-				<BreadCrumb topicTitle={topicTitle} topicId={topicId} resourceId={resourceId} resourceTitle={resourceTitle}/>
+				<Breadcrumbs
+					topicTitle={topic.title}
+					topicId={topic.id}
+					resourceId={resource.id}
+					resourceTitle={resource.title}
+				/>
 				<div className="learn">
 					{/* TODO: Conditional logic to render a different card/layout for state.learn.resource.type === other resources */}
 					{/* TODO: Change this to be dynamic for state.learn.resource.type === youtube*/}
@@ -66,10 +70,8 @@ const mapStateToProps = (state, props) => {
 		loading: state.learn.loading,
 		resourceNotFound: state.learn.error && state.learn.error.status === 404,
 		notebook: state.learn.topic && (state.learn.topic.notebook || ''),
-		resourceId: currentResource && currentResource.id,
-		resourceTitle: currentResource && currentResource.title,
-		topicTitle: currentTopic&& currentTopic.title,
-		topicId: currentTopic && currentTopic.id
+		resource: currentResource,
+		topic: currentTopic
 	};
 };
 
