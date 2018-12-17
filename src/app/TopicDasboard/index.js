8587448cf479ec5190a29dc6773cf8b8943f6be5
continuouslylearning <40921964+continuouslylearning@users.noreply.github.com>
2018-12-17 08:02:25 -0800

    creates breadcrumb component and adds it to topic and resource dashboard (#27)
    * creates breadcrumb component and adds it to topic and resource dashboard
    
    * Small style fixes

--- src/app/TopicDasboard/index.js
+++ src/app/TopicDasboard/index.js
@@ -5,35 +5,40 @@ import CompletedResourceContainer from './CompletedResourceContainer';
 import { getResources } from '../../controller/actions/topicDashboard';
 import Loading from '../common/Loading';
 import './index.scss';
+import Breadcrumbs from '../common/Breadcrumbs';
+
 export class TopicDashboard extends React.Component {
-  componentDidMount() {
-    const id = this.props.match.params.topicId;
-    this.props.dispatch(getResources(id));
-  }
+	componentDidMount() {
+		const id = this.props.match.params.topicId;
+		this.props.dispatch(getResources(id));
+	}
 
-  render() {
-    if (this.props.loading) {
-      return <Loading />;
-    }
-    return (
-      <main className="topic-dashboard">
-        <section>
-          <h2>Breadcrumb nav placeholder</h2>
-        </section>
-        <h2>Active Resources</h2>
-        <ActiveResourceContainer resources={this.props.resources} />
+	render() {
+		if (this.props.loading) {
+			return <Loading />;
+		}
+		return (
+			<main className="topic-dashboard">
+				<section>
+					<h2>
+						<Breadcrumbs topicId={this.props.parentId} topicTitle={this.props.topicTitle} />
+					</h2>
+				</section>
+				<h2>Active Resources</h2>
+				<ActiveResourceContainer resources={this.props.resources} />
 
-        <h2>Completed Resources </h2>
-        <CompletedResourceContainer resources={this.props.resources} />
-      </main>
-    );
-  }
+				<h2>Completed Resources </h2>
+				<CompletedResourceContainer resources={this.props.resources} />
+			</main>
+		);
+	}
 }
 const mapStateToProps = state => {
-  return {
-    loading: state.topicDashReducer.loading,
-    parentId: state.topicDashReducer.topicId,
-    resources: state.topicDashReducer.resources
-  };
+	return {
+		loading: state.topicDashReducer.loading,
+		parentId: state.topicDashReducer.topicID,
+		resources: state.topicDashReducer.resources,
+		topicTitle: state.topicDashReducer.topicTitle
+	};
 };
 export default connect(mapStateToProps)(TopicDashboard);
