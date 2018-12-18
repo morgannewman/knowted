aca33394af063c4bcbeab114a198acf4154883e0
Morgan Newman <mpnewman95@gmail.com>
2018-12-18 09:33:09 -0800

    Adds droppable to jawbone

--- src/app/Dashboard/JawBone.js
+++ src/app/Dashboard/JawBone.js
@@ -1,31 +1,48 @@
+import './JawBone.css';
 import React from 'react';
 import Topic from './Topic';
-import './JawBone.css';
+
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 import Loading from '../common/Loading';
 
 import { connect } from 'react-redux';
 
 export class JawBone extends React.Component {
-  render() {
-    if (this.props.loading) return <Loading />;
-    const { topics } = this.props;
-
-    return (
-      <div className="jaw-bone-container">
-        {topics.map(topic =>
-          topic.parent && topic.parent.id === this.props.folderId ? (
-            <Topic title={topic.title} key={topic.id} topicId={topic.id} />
-          ) : null
-        )}
-      </div>
-    );
-  }
+	render() {
+		if (this.props.loading) return <Loading />;
+		const { topics } = this.props;
+		return (
+			<div className="jaw-bone-container">
+				<Droppable droppableId={this.props.folderId} direction="row" isCombineEnabled={false}>
+					{provided => (
+						<div className="jawbone-folder-items" ref={provided.innerRef} {...provided.droppableProps}>
+							{topics.map((topic, index) =>
+								topic.parent && topic.parent.id === this.props.folderId ? (
+									<Draggable draggableId={topic.id} index={index} key={topic.id}>
+										{provided => (
+											<div
+												ref={provided.innerRef}
+												{...provided.draggableProps}
+												{...provided.dragHandleProps}
+											>
+												<Topic title={topic.title} topicId={topic.id} />
+											</div>
+										)}
+									</Draggable>
+								) : null
+							)}
+						</div>
+					)}
+				</Droppable>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => ({
-  topics: state.dashboardReducer.topics,
-  loading: state.dashboardReducer.loading
+	topics: state.dashboardReducer.topics,
+	loading: state.dashboardReducer.loading
 });
 
 export default connect(mapStateToProps)(JawBone);
