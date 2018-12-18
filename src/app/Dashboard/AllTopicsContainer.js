b85074add46d008b2d75ebc654014053faa0dc57
Morgan Newman <mpnewman95@gmail.com>
2018-12-18 14:04:47 -0800

    Adds ability to drag in and out of jawbone (#34)
    * Adds droppable to jawbone
    
    * Adds support to drag into folders
    
    * Adds adds support to drag out from folders

--- src/app/Dashboard/AllTopicsContainer.js
+++ src/app/Dashboard/AllTopicsContainer.js
@@ -8,136 +8,116 @@ import AddTopic from './AddTopic';
 import Folder from './Folder';
 import Topic from './Topic';
 
-import { mergeTopicsNewFolder } from '../../controller/actions/dashboard';
+import { mergeTopicsNewFolder, updateTopic } from '../../controller/actions/dashboard';
 
 export class AllTopicsContainer extends React.Component {
-  static propTypes = {
-    topics: PropTypes.array,
-    folders: PropTypes.array
-  };
+	static propTypes = {
+		topics: PropTypes.array,
+		folders: PropTypes.array
+	};
 
-  combine = (topicId1, topicId2) => {
-    this.props.dispatch(
-      mergeTopicsNewFolder(`Untitled Folder ${Date.now()}`, topicId1, topicId2)
-    );
-  };
+	handleTopicCombine = (topicId1, topicId2) => {
+		this.props.dispatch(mergeTopicsNewFolder(`Untitled Folder-${Date.now()}`, topicId1, topicId2));
+	};
 
-  // reorder = (list, startIndex, endIndex) => {
-  //   const result = Array.from(list);
-  //   const [removed] = result.splice(startIndex, 1);
-  //   result.splice(endIndex, 0, removed);
-  //   return result;
-  // };
+	handleTopicFolderChange = result => {
+		const { destination, draggableId } = result;
+		// update folder
+		const parent = destination.droppableId === 'lonelyTopics' ? null : Number(destination.droppableId);
+		this.props.dispatch(updateTopic({ id: Number(draggableId), parent }));
+	};
 
-  onDragEnd = result => {
-    const { destination, source, draggableId, combine } = result;
+	onDragEnd = result => {
+		console.log(result);
 
-    //CASE: combining lone topics => creates a new folder and places items within that folder
-    if (combine) {
-      this.combine(combine.draggableId, draggableId);
-    }
+		const { destination, source, draggableId, combine } = result;
 
-    // CASE: not reordered
-    if (
-      !destination ||
-      (destination.droppableId === source.droppableId &&
-        destination.index === source.index)
-    ) {
-      return;
-    }
+		//CASE: combining lone topics => creates a new folder and places items within that folder
+		if (combine) {
+			this.handleTopicCombine(combine.draggableId, draggableId);
+		}
 
-    // TODO: CASE: Move to other list (different droppable id)
-    // removing last item from folder => deletes folder
+		// CASE: not reordered
+		if (!destination || (destination.droppableId === source.droppableId && destination.index === source.index)) {
+			return;
+		}
 
-    // //reorder
-    // const topics = this.reorder(
-    //   this.props.topicOrder,
-    //   source.index,
-    //   destination.index
-    // );
+		// TODO: CASE: Move to other list (different droppable id)
+		if (source.droppableId !== destination.droppableId) {
+			this.handleTopicFolderChange(result);
+		}
+		// removing last item from folder => deletes folder
+	};
 
-    // this.props.dispatch(updateTopicOrder(topics, this.props.userId));
-  };
+	render() {
+		const { topics, folders } = this.props;
+		return (
+			<DragDropContext onDragEnd={this.onDragEnd}>
+				<section className="all-topics-container">
+					<div className="folders-container">
+						{folders &&
+							folders.map((folder, index) => {
+								return (
+									<Folder
+										title={folder.title}
+										folderId={folder.id}
+										key={folder.id}
+										index={index}
+										editing={
+											folder.id === this.props.currentFolderId ? this.props.currentFolderId : null
+										}
+									/>
+								);
+							})}
+					</div>
+					<div className="lonely-topics-container">
+						<AddTopic />
+						<Droppable droppableId="lonelyTopics" direction="horizontal" isCombineEnabled>
+							{provided => (
+								<div ref={provided.innerRef} {...provided.droppableProps}>
+									{topics &&
+										this.props.topics.map(
+											(topic, index) =>
+												(!topic.parent || !topic.parent.id) && (
+													<Draggable key={topic.id} draggableId={topic.id} index={index}>
+														{provided => (
+															<div
+																ref={provided.innerRef}
+																{...provided.draggableProps}
+																{...provided.dragHandleProps}
+															>
+																<Topic
+																	title={topic.title}
+																	topicId={topic.id}
+																	key={topic.id}
+																	index={index}
+																/>
+															</div>
+														)}
+													</Draggable>
+												)
+										)}
 
-  render() {
-    const { topics, folders } = this.props;
-    return (
-      <section className="all-topics-container">
-        <div className="folders-container">
-          {folders &&
-            folders.map((folder, index) => {
-              return (
-                <Folder
-                  title={folder.title}
-                  folderId={folder.id}
-                  key={folder.id}
-                  index={index}
-                  editing={
-                    folder.id === this.props.currentFolderId
-                      ? this.props.currentFolderId
-                      : null
-                  }
-                />
-              );
-            })}
-        </div>
-        <div className="lonely-topics-container">
-          <AddTopic />
-          <DragDropContext onDragEnd={this.onDragEnd}>
-            <Droppable
-              droppableId="lonelyTopics"
-              direction="horizontal"
-              isCombineEnabled
-            >
-              {provided => (
-                <div ref={provided.innerRef} {...provided.droppableProps}>
-                  {topics &&
-                    this.props.topics.map(
-                      (topic, index) =>
-                        (!topic.parent || !topic.parent.id) && (
-                          <Draggable
-                            key={topic.id}
-                            draggableId={topic.id}
-                            index={index}
-                          >
-                            {provided => (
-                              <div
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                              >
-                                <Topic
-                                  title={topic.title}
-                                  topicId={topic.id}
-                                  key={topic.id}
-                                  index={index}
-                                />
-                              </div>
-                            )}
-                          </Draggable>
-                        )
-                    )}
-
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-          </DragDropContext>
-        </div>
-      </section>
-    );
-  }
+									{provided.placeholder}
+								</div>
+							)}
+						</Droppable>
+					</div>
+				</section>
+			</DragDropContext>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-  // console.log(state.dashboardReducer);
-  return {
-    topics: state.dashboardReducer.topics,
-    // topicOrder: state.dashboardReducer.topicOrder,
-    folders: state.dashboardReducer.folders,
-    currentFolderId: state.dashboardReducer.currentFolderId,
-    userId: state.auth.user.id
-  };
+	// console.log(state.dashboardReducer);
+	return {
+		topics: state.dashboardReducer.topics,
+		// topicOrder: state.dashboardReducer.topicOrder,
+		folders: state.dashboardReducer.folders,
+		currentFolderId: state.dashboardReducer.currentFolderId,
+		userId: state.auth.user.id
+	};
 };
 
 export default connect(mapStateToProps)(AllTopicsContainer);
