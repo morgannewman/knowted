c77e055f7be95f3326abbf060f883336c044683e
Morgan Newman <mpnewman95@gmail.com>
2018-12-18 07:50:15 -0800

    Fixes broken build

--- src/app/Dashboard/AllTopicsContainer.js
+++ src/app/Dashboard/AllTopicsContainer.js
@@ -8,141 +8,122 @@ import AddTopic from './AddTopic';
 import Folder from './Folder';
 import Topic from './Topic';
 
-import {
-  updateTopicOrder,
-  mergeTopicsNewFolder
-} from '../../controller/actions/dashboard';
+import { mergeTopicsNewFolder } from '../../controller/actions/dashboard';
 
 export class AllTopicsContainer extends React.Component {
-  static propTypes = {
-    topics: PropTypes.array,
-    folders: PropTypes.array
-  };
+	static propTypes = {
+		topics: PropTypes.array,
+		folders: PropTypes.array
+	};
 
-  combine = (topicId1, topicId2) => {
-    this.props.dispatch(
-      mergeTopicsNewFolder(`Untitled Folder ${Date.now()}`, topicId1, topicId2)
-    );
-  };
+	combine = (topicId1, topicId2) => {
+		this.props.dispatch(mergeTopicsNewFolder(`Untitled Folder ${Date.now()}`, topicId1, topicId2));
+	};
 
-  // reorder = (list, startIndex, endIndex) => {
-  //   const result = Array.from(list);
-  //   const [removed] = result.splice(startIndex, 1);
-  //   result.splice(endIndex, 0, removed);
-  //   return result;
-  // };
+	// reorder = (list, startIndex, endIndex) => {
+	//   const result = Array.from(list);
+	//   const [removed] = result.splice(startIndex, 1);
+	//   result.splice(endIndex, 0, removed);
+	//   return result;
+	// };
 
-  onDragEnd = result => {
-    const { destination, source, draggableId, combine } = result;
+	onDragEnd = result => {
+		const { destination, source, draggableId, combine } = result;
 
-    //CASE: combining lone topics => creates a new folder and places items within that folder
-    if (combine) {
-      this.combine(combine.draggableId, draggableId);
-    }
+		//CASE: combining lone topics => creates a new folder and places items within that folder
+		if (combine) {
+			this.combine(combine.draggableId, draggableId);
+		}
 
-    // CASE: not reordered
-    if (
-      !destination ||
-      (destination.droppableId === source.droppableId &&
-        destination.index === source.index)
-    ) {
-      return;
-    }
+		// CASE: not reordered
+		if (!destination || (destination.droppableId === source.droppableId && destination.index === source.index)) {
+			return;
+		}
 
-    // TODO: CASE: Move to other list (different droppable id)
-    // removing last item from folder => deletes folder
+		// TODO: CASE: Move to other list (different droppable id)
+		// removing last item from folder => deletes folder
 
-    // //reorder
-    // const topics = this.reorder(
-    //   this.props.topicOrder,
-    //   source.index,
-    //   destination.index
-    // );
+		// //reorder
+		// const topics = this.reorder(
+		//   this.props.topicOrder,
+		//   source.index,
+		//   destination.index
+		// );
 
-    // this.props.dispatch(updateTopicOrder(topics, this.props.userId));
-  };
+		// this.props.dispatch(updateTopicOrder(topics, this.props.userId));
+	};
 
-  render() {
-    const { topics, folders } = this.props;
-    return (
-      <section className="all-topics-container">
-        <div className="folders-container">
-          {folders &&
-            folders.map((folder, index) => {
-              return (
-                <Folder
-                  title={folder.title}
-                  folderId={folder.id}
-                  key={folder.id}
-                  index={index}
-                  editing={
-                    folder.id === this.props.currentFolderId
-                      ? this.props.currentFolderId
-                      : null
-                  }
-                />
-              );
-            })}
-        </div>
-        <div className="lonely-topics-container">
-          <AddTopic />
-          <DragDropContext onDragEnd={this.onDragEnd}>
-            <Droppable
-              droppableId="lonelyTopics"
-              direction="horizontal"
-              isCombineEnabled
-            >
-              {provided => (
-                <div ref={provided.innerRef} {...provided.droppableProps}>
-                  {topics &&
-                    this.props.topics
-                      // this.state.topicOrder
-                      .map(
-                        (topic, index) =>
-                          (!topic.parent || !topic.parent.id) && (
-                            <Draggable
-                              key={topic.id}
-                              draggableId={topic.id}
-                              index={index}
-                            >
-                              {provided => (
-                                <div
-                                  ref={provided.innerRef}
-                                  {...provided.draggableProps}
-                                  {...provided.dragHandleProps}
-                                >
-                                  <Topic
-                                    title={topic.title}
-                                    topicId={topic.id}
-                                    key={topic.id}
-                                    index={index}
-                                  />
-                                </div>
-                              )}
-                            </Draggable>
-                          )
-                      )}
+	render() {
+		const { topics, folders } = this.props;
+		return (
+			<section className="all-topics-container">
+				<div className="folders-container">
+					{folders &&
+						folders.map((folder, index) => {
+							return (
+								<Folder
+									title={folder.title}
+									folderId={folder.id}
+									key={folder.id}
+									index={index}
+									editing={
+										folder.id === this.props.currentFolderId ? this.props.currentFolderId : null
+									}
+								/>
+							);
+						})}
+				</div>
+				<div className="lonely-topics-container">
+					<AddTopic />
+					<DragDropContext onDragEnd={this.onDragEnd}>
+						<Droppable droppableId="lonelyTopics" direction="horizontal" isCombineEnabled>
+							{provided => (
+								<div ref={provided.innerRef} {...provided.droppableProps}>
+									{topics &&
+										this.props.topics
+											// this.state.topicOrder
+											.map(
+												(topic, index) =>
+													(!topic.parent || !topic.parent.id) && (
+														<Draggable key={topic.id} draggableId={topic.id} index={index}>
+															{provided => (
+																<div
+																	ref={provided.innerRef}
+																	{...provided.draggableProps}
+																	{...provided.dragHandleProps}
+																>
+																	<Topic
+																		title={topic.title}
+																		topicId={topic.id}
+																		key={topic.id}
+																		index={index}
+																	/>
+																</div>
+															)}
+														</Draggable>
+													)
+											)}
 
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-          </DragDropContext>
-        </div>
-      </section>
-    );
-  }
+									{provided.placeholder}
+								</div>
+							)}
+						</Droppable>
+					</DragDropContext>
+				</div>
+			</section>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-  // console.log(state.dashboardReducer);
-  return {
-    topics: state.dashboardReducer.topics,
-    // topicOrder: state.dashboardReducer.topicOrder,
-    folders: state.dashboardReducer.folders,
-    currentFolderId: state.dashboardReducer.currentFolderId,
-    userId: state.auth.user.id
-  };
+	// console.log(state.dashboardReducer);
+	return {
+		topics: state.dashboardReducer.topics,
+		// topicOrder: state.dashboardReducer.topicOrder,
+		folders: state.dashboardReducer.folders,
+		currentFolderId: state.dashboardReducer.currentFolderId,
+		userId: state.auth.user.id
+	};
 };
 
 export default connect(mapStateToProps)(AllTopicsContainer);
--- src/app/Dashboard/Folder.js
+++ src/app/Dashboard/Folder.js
@@ -4,102 +4,88 @@ import PropTypes from 'prop-types';
 import './Folder.css';
 import { connect } from 'react-redux';
 
-import {
-  updateFolder,
-  displayEditFolderForm,
-  hideEditFolderForm
-} from '../../controller/actions/dashboard';
+import { updateFolder, displayEditFolderForm, hideEditFolderForm } from '../../controller/actions/dashboard';
 
 export class Folder extends React.Component {
-  static propTypes = {
-    folderId: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired
-  };
+	static propTypes = {
+		folderId: PropTypes.number.isRequired,
+		title: PropTypes.string.isRequired
+	};
 
-  state = {
-    showJawBone: false,
-    showEdit: false
-  };
+	state = {
+		showJawBone: false,
+		showEdit: false
+	};
 
-  //CASE: toggle jawbone based on clicking on folder
-  toggleJawBone = () => {
-    this.setState(state => {
-      return { showJawBone: !state.showJawBone };
-    });
-  };
+	//CASE: toggle jawbone based on clicking on folder
+	toggleJawBone = () => {
+		this.setState(state => {
+			return { showJawBone: !state.showJawBone };
+		});
+	};
 
-  //CASE: display and hide edit option for folders on mouse ender and leave
-  displayEdit = () => {
-    this.setState(() => {
-      return { showEdit: true };
-    });
-  };
+	//CASE: display and hide edit option for folders on mouse ender and leave
+	displayEdit = () => {
+		this.setState(() => {
+			return { showEdit: true };
+		});
+	};
 
-  hideEdit = () => {
-    this.setState(() => {
-      return { showEdit: false };
-    });
-  };
+	hideEdit = () => {
+		this.setState(() => {
+			return { showEdit: false };
+		});
+	};
 
-  //CASE: display and hide folder form based on clicking edit or cancel btn
-  displayEditFolderForm = () => {
-    this.props.dispatch(displayEditFolderForm(this.props.folderId));
-  };
+	//CASE: display and hide folder form based on clicking edit or cancel btn
+	displayEditFolderForm = () => {
+		this.props.dispatch(displayEditFolderForm(this.props.folderId));
+	};
 
-  hideEditFolderForm = () => {
-    this.props.dispatch(hideEditFolderForm(this.props.folderId));
-  };
+	hideEditFolderForm = () => {
+		this.props.dispatch(hideEditFolderForm(this.props.folderId));
+	};
 
-  submitEdit = e => {
-    e.preventDefault();
-    let title = this.titleInput.value;
-    this.props.dispatch(updateFolder(title, this.props.folderId));
-    this.hideEditFolderForm();
-  };
+	submitEdit = e => {
+		e.preventDefault();
+		let title = this.titleInput.value;
+		this.props.dispatch(updateFolder(title, this.props.folderId));
+		this.hideEditFolderForm();
+	};
 
-  render() {
-    const { title, folderId } = this.props;
-<<<<<<< HEAD
-
-=======
->>>>>>> master
-    return (
-      <div
-        className="folder-wrap"
-        onMouseEnter={this.displayEdit}
-        onMouseLeave={this.hideEdit}
-      >
-        {this.props.editing ? (
-          <>
-            <button onClick={this.hideEditFolderForm}>Cancel</button>
-            <form className="edit-folder-form" onSubmit={this.submitEdit}>
-              <label>Folder Name</label>
-              <input
-                ref={input => (this.titleInput = input)}
-                type="text"
-                name="folderTitle"
-                defaultValue={this.props.title}
-              />
-            </form>
-          </>
-        ) : (
-          <>
-            {this.state.showEdit && (
-              <button onClick={this.displayEditFolderForm}>Edit</button>
-            )}
-            <button className="folder-btn" onClick={this.toggleJawBone}>
-              {title}
-            </button>
-            {this.state.showJawBone && <JawBone folderId={folderId} />}
-          </>
-        )}
-      </div>
-    );
-  }
+	render() {
+		const { title, folderId } = this.props;
+		return (
+			<div className="folder-wrap" onMouseEnter={this.displayEdit} onMouseLeave={this.hideEdit}>
+				{this.props.editing ? (
+					<>
+						<button onClick={this.hideEditFolderForm}>Cancel</button>
+						<form className="edit-folder-form" onSubmit={this.submitEdit}>
+							<label>Folder Name</label>
+							<input
+								ref={input => (this.titleInput = input)}
+								type="text"
+								name="folderTitle"
+								defaultValue={this.props.title}
+							/>
+						</form>
+					</>
+				) : (
+					<>
+						{this.state.showEdit && <button onClick={this.displayEditFolderForm}>Edit</button>}
+						<button className="folder-btn" onClick={this.toggleJawBone}>
+							{title}
+						</button>
+						{this.state.showJawBone && <JawBone folderId={folderId} />}
+					</>
+				)}
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => ({
-  folders: state.dashboardReducer.folders
+	folders: state.dashboardReducer.folders
 });
 
 export default connect(mapStateToProps)(Folder);
