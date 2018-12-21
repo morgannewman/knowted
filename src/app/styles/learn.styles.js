a438a890254e2967be12f714e99ab7d345b9de95
Morgan Newman <mpnewman95@gmail.com>
2018-12-20 18:39:23 -0800

    Improves YouTube layout to be mobile responsive

--- src/app/styles/learn.styles.js
+++ src/app/styles/learn.styles.js
@@ -15,9 +15,9 @@ export const Main = styled.main`
   display: flex;
   flex: 1;
   flex-basis: 100%;
+  flex-direction: column;
 
   &.single {
-    flex-direction: column;
     width: 98%;
     max-width: 768px;
     margin: 0 auto;
@@ -28,11 +28,19 @@ export const Main = styled.main`
   }
 
   &.double {
-    flex-direction: row;
     .editor,
     .ql-editor {
       min-width: 50vw;
-      min-height: 83vh;
+      min-height: 40vh;
+    }
+    @media (min-width: 1025px) {
+      &.double {
+        flex-direction: row;
+        .editor,
+        .ql-editor {
+          min-height: 83vh;
+        }
+      }
     }
   }
 `;
@@ -44,10 +52,12 @@ export const Card = styled.a`
 
 export const YouTube = styled.iframe`
   min-width: 50vw;
+  @media (max-width: 1024px) {
+    height: 40vh;
+  }
 `;
 
 export const Editor = styled.div`
-  height: 100%;
   overflow-x: hidden;
   overflow-y: scroll;
 `;
