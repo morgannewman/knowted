9a346f53273802a881bb0ac71c12c16ad8591dea
Morgan Newman <mpnewman95@gmail.com>
2018-12-21 10:27:19 -0800

    Changes demo account to create a new user every time

--- src/app/Landing/Demo/index.js
+++ src/app/Landing/Demo/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { submitAuthLogin } from '../../../controller/actions/auth';
+import { submitAuthRegistration } from '../../../controller/actions/auth';
 
 class Demo extends React.Component {
   render() {
     const { loggedIn, dispatch } = this.props;
     if (loggedIn) return <Redirect to="/dashboard" />;
     dispatch(
-      submitAuthLogin({ email: 'oliver@test.com', password: 'testing123' })
+      submitAuthRegistration(
+        {
+          name: 'Demo User',
+          email: `${Date.now()}@email.com`,
+          password: 'password'
+        },
+        { demo: true }
+      )
     );
     return <Redirect to="/dashboard" />;
   }
