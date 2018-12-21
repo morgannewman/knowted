0b18f9394052d4fa784a435ecbe494f40a938432
Morgan Newman <mpnewman95@gmail.com>
2018-12-21 10:29:48 -0800

    Demo account (#51)
    * Removes withNav HOC
    
    * Adds demo account route

--- src/controller/api/routes/auth.route.js
+++ src/controller/api/routes/auth.route.js
@@ -6,49 +6,54 @@ const BASE_URL = '/auth';
  * This object exposes auth endpoints from the backend.
  */
 export default {
-	/**
-	 * Registers a user with the backend server.
-	 * @param {{name: string, email: string, password: string}} credentials
-	 * @returns {{username: string, createdAt: string, updatedAt: string}}
-	 * @throws if user is not properly authenticated
-	 * @throws if any fields are missing
-	 */
-	register(credentials) {
-		return goFetch('/api/users', {
-			method: 'POST',
-			body: credentials,
-			auth: false
-		}).then(res => {
-			cache.authToken.save(res.authToken);
-			return res;
-		});
-	},
+  /**
+   * Registers a user with the backend server.
+   * @param {{name: string, email: string, password: string}} credentials
+   * @param {{}} params
+   * @returns {{username: string, createdAt: string, updatedAt: string}}
+   * @throws if user is not properly authenticated
+   * @throws if any fields are missing
+   */
+  register(credentials, params = null) {
+    return goFetch(
+      '/api/users',
+      {
+        method: 'POST',
+        body: credentials,
+        auth: false
+      },
+      params
+    ).then(res => {
+      cache.authToken.save(res.authToken);
+      return res;
+    });
+  },
 
-	/**
-	 * Logs in to the backend server and stores token for future requests.
-	 * @param {{email: string, password: string}} credentials
-	 * @returns {{name: string, createdAt: string, updatedAt: string}}
-	 * @throws if user is not properly authenticated
-	 */
-	login(credentials) {
-		return goFetch(BASE_URL + '/login', {
-			method: 'POST',
-			body: credentials,
-			auth: false
-		}).then(res => {
-			cache.authToken.save(res.authToken);
-			return res;
-		});
-	},
+  /**
+   * Logs in to the backend server and stores token for future requests.
+   * @param {{email: string, password: string}} credentials
+   * @returns {{name: string, createdAt: string, updatedAt: string}}
+   * @throws if user is not properly authenticated
+   */
+  login(credentials) {
+    return goFetch(BASE_URL + '/login', {
+      method: 'POST',
+      body: credentials,
+      auth: false
+    }).then(res => {
+      cache.authToken.save(res.authToken);
+      return res;
+    });
+  },
 
-	/**
-	 * Exchanges auth token for new auth token.
-	 * @returns {Boolean} true/false if auth succeeded and token is available for queries.
-	 */
-	refresh() {
-		return goFetch(BASE_URL + '/refresh', { method: 'POST' }).then(res => {
-			cache.authToken.save(res.authToken);
-			return res;
-		});
-	}
+  /**
+   * Exchanges auth token for new auth token.
+   * @returns {Boolean} true/false if auth succeeded and token is available for queries.
+   */
+  refresh() {
+    return goFetch(BASE_URL + '/refresh', { method: 'POST' }).then(res => {
+      cache.authToken.save(res.authToken);
+      return res;
+    });
+  }
 };
